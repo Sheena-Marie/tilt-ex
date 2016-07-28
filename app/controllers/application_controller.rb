2e3bc2da6a6de414f893a68609cac7108801a2b1
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-07-28 15:17:16 +1000

    added gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  response = HTTParty.get('https://www.tilt.com/tilts/lets-get-allysa-to-the-rio-olympics/stats.json')
+
+  puts response.body, response.code, response.message, response.headers.inspect
+
 end
