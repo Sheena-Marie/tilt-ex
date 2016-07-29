51a1aadba1129b62d498a9896870d8129297489b
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-07-29 17:14:35 +1000

    fixed some things in the timer partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
 
 
   response = HTTParty.get('https://www.tilt.com/tilts/lets-get-allysa-to-the-rio-olympics/stats.json')
-
   puts response.body, response.code, response.message, response.headers.inspect
 
 end
